5900efd9369bd79e5f44b963834c841cc71deff7
Rishvik Kambhampati <51458941+rishvikk11@users.noreply.github.com>
2026-09-17 00:19:56 -0400

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        # dynamic sliding window solution, k not given, need to find a minimum subarray
+        l, currSum = 0, 0
+        min_window = float('inf')
+
+        for r in range(len(nums)):
+            currSum += nums[r]
+            while currSum >= target:
+                min_window = min(min_window, r-l+1)
+                currSum -= nums[l]
+                l += 1
+            
+        if min_window == float('inf'):
+            return 0
+        return min_window
+
+            
